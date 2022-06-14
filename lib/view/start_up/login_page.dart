e82eeee305ba1795d4a35e8f6a9f16afc3f7c546
shinji hayamizu <max9173627@gmail.com>
2022-06-14 13:41:13 +0900

    ログインページの登録バリデーションの追加

--- lib/view/start_up/login_page.dart
+++ lib/view/start_up/login_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:studysharesns/view/post/posts_index_page.dart';
 import 'package:studysharesns/view/screen.dart';
 
+import 'create_account.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -19,63 +21,66 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
       child: Scaffold(
           body: SizedBox(
-            width: double.infinity,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  "エンジニア用SNS(仮)",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                      width: 300,
-                      child: TextField(
-                        controller: emailController,
-                        decoration: InputDecoration(hintText: "メールアドレス"),
-                      )),
-                ),
-                SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: passController,
-                      decoration: InputDecoration(hintText: "パスワード"),
-                    )),
-                const SizedBox(
-                  height: 10,
-                ),
-                RichText(
-                    text:
-                    TextSpan(style: const TextStyle(color: Colors.black), children: [
-                      const TextSpan(text: "アカウントを作成していない方は"),
-                      TextSpan(
-                          text: "こちら",
-                          style: const TextStyle(color: Colors.blue),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              // Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //       builder: (context) => CreateAccountPage(),
-                              //     ));
-                            })
-                    ])),
-                const SizedBox(
-                  height: 70,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      // pushReplacementで画面を
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => const Screen()));
-                    },
-                    child: const Text("emailログイン"))
-              ],
+        width: double.infinity,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          )),
+            const Text(
+              "エンジニア用SNS(仮)",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: "メールアドレス"),
+                  )),
+            ),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: passController,
+                  decoration: InputDecoration(hintText: "パスワード"),
+                )),
+            const SizedBox(
+              height: 10,
+            ),
+            RichText(
+                text: TextSpan(
+                    style: const TextStyle(color: Colors.black),
+                    children: [
+                  const TextSpan(text: "アカウントを作成していない方は"),
+                  TextSpan(
+                      text: "こちら",
+                      style: const TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const CreateAccountPage(),
+                              ));
+                        })
+                ])),
+            const SizedBox(
+              height: 70,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (emailController.text.isNotEmpty &&
+                      passController.text.isNotEmpty) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => const Screen()));
+                  }
+                },
+                child: const Text("emailログイン"))
+          ],
+        ),
+      )),
     );
   }
 }
