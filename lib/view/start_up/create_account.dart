fe131df4456ad59170a7707b46f28fba1b69cd08
shinji hayamizu <max9173627@gmail.com>
2022-06-14 13:40:56 +0900

    アカウント作成ページの作成

---
+++ lib/view/start_up/create_account.dart
@@ -0,0 +1,107 @@
+
+import 'package:flutter/material.dart';
+
+import '../screen.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({Key? key}) : super(key: key);
+
+  @override
+  _CreateAccountPageState createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController userIdController = TextEditingController();
+  TextEditingController selfIntroductionController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Colors.black),
+          title: const Text(
+            "アカウント登録",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                const CircleAvatar(
+                  radius: 40,
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(hintText: "名前"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: userIdController,
+                      decoration: const InputDecoration(hintText: "ユーザーID"),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: selfIntroductionController,
+                    decoration: const InputDecoration(hintText: "自己紹介"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                          hintText: "メールアドレス"
+                              ""),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: passController,
+                    decoration: const InputDecoration(hintText: "パスワード"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (nameController.text.isNotEmpty &&
+                        userIdController.text.isNotEmpty &&
+                        selfIntroductionController.text.isNotEmpty &&
+                        emailController.text.isNotEmpty &&
+                        passController.text.isNotEmpty) {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => const Screen()));
+                    }
+                  },
+                  child: const Text("アカウント作成"),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
