6e7d519a1d3cdf2d6c2a4a9bc742aba37ac32119
shinji hayamizu <max9173627@gmail.com>
2022-06-15 13:30:22 +0900

    アプリとFirebaseを連携、Firebaseの初期化処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:studysharesns/view/post/posts_index_page.dart';
 import 'package:studysharesns/view/screen.dart';
 import 'package:studysharesns/view/start_up/login_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
