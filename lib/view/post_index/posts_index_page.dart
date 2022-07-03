007af9647aab8568aad791e206d670ccfa97a45c
shinji hayamizu <max9173627@gmail.com>
2022-07-03 14:20:40 +0900

    投稿一覧の最後の投稿UI表示不具合の解消

--- lib/view/post_index/posts_index_page.dart
+++ lib/view/post_index/posts_index_page.dart
@@ -28,16 +28,9 @@ class PostIndexPage extends HookConsumerWidget {
               automaticallyImplyLeading: false),
           body: accountList.when(
             data: (accountList) => ListView.builder(
-              itemCount: postList.value?.length,
+              itemCount: postList.value!.length + 1,
               itemBuilder: (context, index) {
-                final post = postList.value?[index];
-                late Account postAccount;
-                for (var account in accountList) {
-                  if (account.id == post?.postAccountId) {
-                    postAccount = account;
-                  }
-                }
-                if (index + 1 == postList.value?.length) {
+                if (index == postList.value?.length) {
                   return Container(
                     alignment: Alignment.center,
                     child: const Padding(
@@ -48,56 +41,64 @@ class PostIndexPage extends HookConsumerWidget {
                       ),
                     ),
                   );
-                }
-                return Container(
-                  decoration: BoxDecoration(
-                    border: index == 0
-                        ? const Border(
-                            top: BorderSide(color: Colors.grey, width: 0),
-                            bottom: BorderSide(color: Colors.grey, width: 0),
-                          )
-                        : const Border(
-                            bottom: BorderSide(color: Colors.grey, width: 0),
-                          ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 22,
-                        foregroundImage: NetworkImage(postAccount.imagePath),
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text(postAccount.name),
-                                        Text("@${postAccount.userId}",
-                                            style: const TextStyle(
-                                                color: Colors.grey)),
-                                      ],
-                                    ),
-                                    Text(
-                                      DateFormat("M/d/yy")
-                                          .format(post!.createdAt),
-                                    ),
-                                  ]),
-                              Text(post.content),
-                            ],
+                } else {
+                  final post = postList.value?[index];
+                  late Account postAccount;
+                  for (var account in accountList) {
+                    if (account.id == post?.postAccountId) {
+                      postAccount = account;
+                    }
+                  }
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: index == 0
+                          ? const Border(
+                              top: BorderSide(color: Colors.grey, width: 0),
+                              bottom: BorderSide(color: Colors.grey, width: 0),
+                            )
+                          : const Border(
+                              bottom: BorderSide(color: Colors.grey, width: 0),
+                            ),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 22,
+                          foregroundImage: NetworkImage(postAccount.imagePath),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(postAccount.name),
+                                          Text("@${postAccount.userId}",
+                                              style: const TextStyle(
+                                                  color: Colors.grey)),
+                                        ],
+                                      ),
+                                      Text(
+                                        DateFormat("M/d/yy")
+                                            .format(post!.createdAt),
+                                      ),
+                                    ]),
+                                Text(post.content),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
+                      ],
+                    ),
+                  );
+                }
               },
             ),
             loading: () => const Center(child: CircularProgressIndicator()),
