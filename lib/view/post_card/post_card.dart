bfd159df2b606a11231b249517f8688889fdc057
shinji hayamizu <max9173627@gmail.com>
2023-01-06 17:46:43 +0900

    ellipsis文字列バグ対応

--- lib/view/post_card/post_card.dart
+++ lib/view/post_card/post_card.dart
@@ -16,6 +16,12 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ellipsisを行う際のバグ対応処理　参考記事 https://qiita.com/sgaku_0514/items/1e03afc37c51844a41a2
+    final String nameAndIdText =
+        Characters('${postAccount.name}@${postAccount.userId}')
+            .replaceAll(Characters(''), Characters('\u{200B}'))
+            .toString();
+
     return Row(
       children: [
         CircleAvatar(
@@ -34,7 +40,7 @@ class PostCard extends StatelessWidget {
                     Flexible(
                       child: Text(
                         maxLines: 1,
-                        "${postAccount.name}@${postAccount.userId}",
+                        nameAndIdText,
                         style: const TextStyle(color: Colors.grey),
                         overflow: TextOverflow.ellipsis,
                       ),
