4b0340f2bd663ea15c742c5a0e4d5e47bf407ab8
shinji hayamizu <max9173627@gmail.com>
2023-01-06 20:00:25 +0900

    軽微な修正

--- lib/view/screen.dart
+++ lib/view/screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:studysharesns/view/post_index/posts_index_page.dart';
 
-import '../samples/methodChannel/battery_level_screen.dart';
+import '../materials/view/item_list_page.dart';
 import 'add_post/add_post_page.dart';
 import 'my_account/my_account_page.dart';
 
